4c5aa7e650bd5088dd998751fa4e7e1cdcc4d4b2
Sumit <sumitkanth999@gmail.com>
2022-03-28 11:54:18 +0530

    set router and middleware

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Middle Ware
+const middileware = (req, res, next) => {
+    console.log("Hello, I am middle ware");
+    next();
+}
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("This is an home page!");
+})
+
+app.get("/about", middileware, (req, res) => {
+    res.send("This is an about page!");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("This is an contact page!");
+})
+
+app.get("/signin", (req, res) => {
+    res.send("This is an signin page!");
+})
+
+app.get("/signup", (req, res) => {
+    res.send("This is an signup page!");
+})
+
+
+// Server
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`)
+})
